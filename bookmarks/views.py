5cbdf4011e6653eb06b1468f08e12a2200d23761
Esam Al Deen <assomy@gmail.com>
2010-10-01 13:03:29 +0300

    modified:   bookmarks/views.py 	modified:   bookmarks/views.pyc 	modified:   bookmarks/views.py~ 	bookmarks/forms.pyc 	bookmarks/forms.py~

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -7,26 +7,24 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
+from django.template import RequestContext
+
+
 
 def main_page(request):
-    return render_to_response('main_page.html',{ 'user': request.user })
+    return render_to_response('main_page.html', RequestContext(request))
+
 
 
 def user_page(request, username):
-  try:
-    user = User.objects.get(username=username)
-  except:
-    raise Http404('Requested user not found.')
+    try:
+        user = User.objects.get(username=username)
+    except:
+        raise Http404('Requested user not found.')
 
-  bookmarks = user.bookmark_set.all()
-
-  template = get_template('user_page.html')
-  variables = Context({
-    'username': username,
-    'bookmarks': bookmarks
-  })
-  output = template.render(variables)
-  return HttpResponse(output)
+    bookmarks = user.bookmark_set.all()
+    variables = RequestContext(request, {'username': username,'bookmarks':bookmarks })
+    return render_to_response('user_page.html', variables)
 
 def logout_page(request):
     logout(request)
