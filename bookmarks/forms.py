efa085c54b83d7db02470805aefc6a38c1a0a42d
Esam Al Deen <assomy@gmail.com>
2010-10-02 14:07:36 +0300

    modified:   bookmarks/forms.py 	modified:   bookmarks/models.py 	modified:   bookmarks/views.py 	modified:   urls.py 	templates/bookmark_save.html 	templates/registration/register.html 	templates/registration/register_success.html

--- bookmarks/forms.py
+++ bookmarks/forms.py
@@ -36,3 +36,20 @@ class RegistrationForm(forms.Form):
                 return password2
             raise forms.ValidationError('Passwords do not match.')
 
+
+class BookmarkSaveForm(forms.Form):
+  url = forms.URLField(
+    label='URL',
+    widget=forms.TextInput(attrs={'size': 64})
+  )
+  title = forms.CharField(
+    label='Title',
+    widget=forms.TextInput(attrs={'size': 64})
+  )
+  tags = forms.CharField(
+    label='Tags',
+    required=False,
+    widget=forms.TextInput(attrs={'size': 64})
+  )
+
+
