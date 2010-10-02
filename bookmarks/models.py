939bf576a1f092e67a36f5644c40a6899797ad74
Esam Al Deen <assomy@gmail.com>
2010-10-02 10:49:21 +0300

    modified:   bookmarks/models.py

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -8,4 +8,7 @@ class Bookmark(models.Model):
   title = models.CharField(max_length=200)
   user = models.ForeignKey(User)
   link = models.ForeignKey(Link)
+class Tag(models.Model):
+  name = models.CharField(max_length=64, unique=True)
+  bookmarks = models.ManyToManyField(Bookmark)
 
